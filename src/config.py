b3b0045ae57fa1a9ff15dc4ed1ebf2164554cafa
Grant George <grant.george@unitedmasters.com>
2024-03-31 07:53:47 -0700

    feat: add secret key

--- src/config.py
+++ src/config.py
@@ -1,22 +1,22 @@
 # src/config.py
 
 
-import os  # new
+import os
 
 
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "my_precious"  # new
+    SECRET_KEY = "my_precious"
 
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # new
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")  # new
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
 class ProductionConfig(BaseConfig):
@@ -26,3 +26,4 @@ class ProductionConfig(BaseConfig):
         url = url.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_DATABASE_URI = url
+    SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")  # new
