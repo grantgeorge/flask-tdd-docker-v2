ac874d738b042715b9de8f0837150841c17c7239
Grant George <grant.george@unitedmasters.com>
2024-03-31 08:29:05 -0700

    fix: lint fixup

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,7 +4,7 @@ from src.api.ping import ping_namespace
 from src.api.users.views import users_namespace
 
 
-api = Api(version="1.0", title="Users API23", doc="/doc")  # updated
+api = Api(version="1.1", title="Users API23", doc="/doc")  # updated
 
 api.add_namespace(ping_namespace, path="/ping")
 api.add_namespace(users_namespace, path="/users")
